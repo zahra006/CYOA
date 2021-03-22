9fcdde55648c49d08ff811f1b38f12aba911ff51
zahra006 <zahratul.ainiakbar@gmail.com>
2021-03-22 15:06:41 +0700

    Initial Commit

--- app/src/main/java/com/mobile/cyoa/LoginActivity.java
+++ app/src/main/java/com/mobile/cyoa/LoginActivity.java
@@ -36,8 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+
             }
         });
 
