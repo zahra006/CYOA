d35c49185c31056141d1938983e9c4dcb8e7d0ee
zahra006 <zahratul.ainiakbar@gmail.com>
2021-04-09 08:19:13 +0700

    Initial Commit

--- app/src/main/java/com/mobile/cyoa/SplashActivity.java
+++ app/src/main/java/com/mobile/cyoa/SplashActivity.java
@@ -2,7 +2,9 @@ package com.mobile.cyoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -19,11 +21,35 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                isFirstTime();
             }
-        },3000);
+
+            private void isFirstTime() {
+                //check if app is running for the first time in forever
+                //save the value to the SharedPreferences
+                SharedPreferences preferences = getApplication().getSharedPreferences("onBoard", Context.MODE_PRIVATE);
+                boolean isFirstTime = preferences.getBoolean("isFirstTime",true);//default value = true
+
+                if (isFirstTime){
+                    //if it's true then it's first time, and we will chage it to false
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("isFirstTime",false);
+                    editor.apply();
+
+                    //then start Auth Activity
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+
+                    //start Home Activity
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        },1500);
     }
 
 }
