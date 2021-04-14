3eff918cdaf8b413c0febaa36238cedf6cfc3862
zahra006 <zahratul.ainiakbar@gmail.com>
2021-04-14 09:24:42 +0700

    Initial Commit

---
+++ app/src/main/java/com/mobile/cyoa/Fragments/AccountFragment.java
@@ -0,0 +1,6 @@
+package com.mobile.cyoa.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class AccountFragment extends Fragment {
+}
---
+++ app/src/main/java/com/mobile/cyoa/Fragments/HomeFragment.java
@@ -0,0 +1,7 @@
+package com.mobile.cyoa.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+}
