7de6bbef25b839ef9a64c088ee7848f01b211483
zahra006 <zahratul.ainiakbar@gmail.com>
2021-04-19 15:05:53 +0700

    Initial Commit

--- app/src/main/java/com/mobile/cyoa/Adapters/CommentsAdapter.java
+++ app/src/main/java/com/mobile/cyoa/Adapters/CommentsAdapter.java
@@ -1,10 +1,13 @@
 package com.mobile.cyoa.Adapters;
 
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.mobile.cyoa.R;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -14,10 +17,17 @@ public class CommentsAdapter {
 
         private CircleImageView imgUserP;
         private TextView txtComment, txtDateComment, txtUsernameComment;
+        private ImageButton imgOption;
 
 
         public CommentsHolder(@NonNull View itemView) {
             super(itemView);
+
+            imgUserP = itemView.findViewById(R.id.imgUserComment);
+            txtComment = itemView.findViewById(R.id.txtComment);
+            txtDateComment = itemView.findViewById(R.id.txtDateComment);
+            txtUsernameComment = itemView.findViewById(R.id.txtUsernameComment);
+            imgOption = itemView.findViewById(R.id.imgOptionComment);
         }
     }
 }
