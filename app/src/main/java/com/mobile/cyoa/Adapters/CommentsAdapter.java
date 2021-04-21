8ad75240226d6945414d1a59f2f4841503efcc14
zahra006 <zahratul.ainiakbar@gmail.com>
2021-04-21 15:08:48 +0700

    Initial Commit

--- app/src/main/java/com/mobile/cyoa/Adapters/CommentsAdapter.java
+++ app/src/main/java/com/mobile/cyoa/Adapters/CommentsAdapter.java
@@ -1,9 +1,7 @@
 package com.mobile.cyoa.Adapters;
 
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,9 +19,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.mobile.cyoa.CommentActivity;
 import com.mobile.cyoa.Constant;
-import com.mobile.cyoa.Fragments.HomeFragment;
 import com.mobile.cyoa.Models.Comment;
 import com.mobile.cyoa.R;
 import com.squareup.picasso.Picasso;
@@ -41,7 +37,7 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
 
     private Context context;
     private ArrayList<Comment> list;
-    private SharedPreferences preferences;
+    private SharedPreferences sharedPreferences;
     private ProgressDialog dialog;
 
     public CommentsAdapter(Context context, ArrayList<Comment> list) {
@@ -49,7 +45,7 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
         this.list = list;
         dialog = new ProgressDialog(context);
         dialog.setCancelable(false);
-        preferences = context.getSharedPreferences("user",Context.MODE_PRIVATE);
+        sharedPreferences = context.getSharedPreferences("user", Context.MODE_PRIVATE);
     }
 
     @NonNull
@@ -67,19 +63,19 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
         holder.txtDate.setText(comment.getDate());
         holder.txtComment.setText(comment.getComment());
 
-        if (preferences.getInt("id",0)!=(comment.getUser().getId())){
+        if (sharedPreferences.getInt("id",0)!=comment.getUser().getId()){
             holder.imgDelete.setVisibility(View.GONE);
-        }
-        else {
+        } else {
             holder.imgDelete.setVisibility(View.VISIBLE);
-            holder.imgDelete.setOnClickListener(v -> {
+        }
+        /*
+        holder.imgDelete.setOnClickListener(v -> {
                 AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 builder.setMessage("Apakah kamu yakin akan menghapus ini?");
                 builder.setPositiveButton("Hapus", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         deleteComment(comment.getId(),position);
-
                     }
                 });
                 builder.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
@@ -89,8 +85,9 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
                     }
                 });
                 builder.show();
-            });
-        }
+        });
+
+         */
 
     }
 
@@ -115,7 +112,7 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
         }){
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
-                String token = preferences.getString("token", "");
+                String token = sharedPreferences.getString("token", "");
                 HashMap<String, String> map = new HashMap<>();
                 map.put("Authorization", "Bearer "+token);
                 return map;
