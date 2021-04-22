a5d7e0b75dfac8c3ce4245bd99ac101691897998
zahra006 <zahratul.ainiakbar@gmail.com>
2021-04-22 11:25:10 +0700

    Initial Commit

--- app/src/main/java/com/mobile/cyoa/HomeActivity.java
+++ app/src/main/java/com/mobile/cyoa/HomeActivity.java
@@ -1,21 +1,59 @@
 package com.mobile.cyoa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+import com.mobile.cyoa.Fragments.AccountFragment;
 import com.mobile.cyoa.Fragments.HomeFragment;
 
 public class HomeActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.frameHomeContainer, new HomeFragment()).commit();
+        fragmentManager.beginTransaction().replace(R.id.frameHomeContainer, new HomeFragment(),HomeFragment.class.getSimpleName()).commit();
+        init();
+    }
 
+    private void init() {
+        navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item_book:{
+                        Fragment account = fragmentManager.findFragmentByTag(AccountFragment.class.getSimpleName());
+                        if (account!=null){
+                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag(AccountFragment.class.getSimpleName())).commit();
+                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag(HomeFragment.class.getSimpleName())).commit();
+                        }
+
+                        break;
+                    }
+                    case R.id.item_account:{
+                        Fragment account = fragmentManager.findFragmentByTag(AccountFragment.class.getSimpleName());
+                        fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag(HomeFragment.class.getSimpleName())).commit();
+                        if (account!=null){
+                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag(AccountFragment.class.getSimpleName())).commit();
+                        }else {
+                            fragmentManager.beginTransaction().add(R.id.frameHomeContainer,new AccountFragment(),AccountFragment.class.getSimpleName()).commit();
+                        }
+
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
     }
 }
