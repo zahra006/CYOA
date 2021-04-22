53d7f7310802c4ecb66ec90b75db9634fe82ee84
zahra006 <zahratul.ainiakbar@gmail.com>
2021-04-22 14:57:31 +0700

    Initial Commit

--- app/src/main/java/com/mobile/cyoa/Fragments/AccountFragment.java
+++ app/src/main/java/com/mobile/cyoa/Fragments/AccountFragment.java
@@ -1,6 +1,26 @@
 package com.mobile.cyoa.Fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.mobile.cyoa.R;
+
 public class AccountFragment extends Fragment {
+    private View view;
+    public AccountFragment (){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.layout_account,container,false);
+        return view;
+    }
 }
