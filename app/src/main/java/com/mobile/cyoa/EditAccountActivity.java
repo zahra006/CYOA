9b9c27df463ef39164e6b81aae95f88896b88a1b
zahra006 <zahratul.ainiakbar@gmail.com>
2021-04-27 14:31:48 +0700

    Initial Commit

--- app/src/main/java/com/mobile/cyoa/EditAccountActivity.java
+++ app/src/main/java/com/mobile/cyoa/EditAccountActivity.java
@@ -2,13 +2,41 @@ package com.mobile.cyoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.mobile.cyoa.Fragments.AccountFragment;
 
 public class EditAccountActivity extends AppCompatActivity {
+
+    private EditText etFirst, etLast, etEmail;
+    private Button btnSaveEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_account);
+        init();
+    }
+
+    private void init() {
+        etFirst = findViewById(R.id.etFirstNameProf);
+        etLast = findViewById(R.id.etLastNameProf);
+        etEmail = findViewById(R.id.etEmailProf);
+        btnSaveEdit = findViewById(R.id.btnSave);
+        editUser();
+    }
+
+    private void editUser() {
+        btnSaveEdit.setOnClickListener(v -> {
+            //POST JSON
+            //Update JSON
+
+
+            //back to profile
+            startActivity(new Intent(EditAccountActivity.this, AccountFragment.class));
+        });
     }
 }
